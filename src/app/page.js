1d2d2198fa82b9734c27157418121daa94f938c6
Yohan Mayorga <yohanucv@gmail.com>
2023-11-30 21:46:11 -0400

    background animated

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import background from "./../../public/background.jpg";
 
 export default function Home() {
   return (
-    <main>
-
-    </main>
-  )
+    <>
+      <div className={styles.background}>
+        <div className={styles.main}>
+          <div className={styles.mainBox}>
+            <div className={styles.titles}>
+              <p className={styles.mainTitle}>SPACE</p>
+              <p className={styles.secondaryTitle}>DEV</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
